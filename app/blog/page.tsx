65458df613422e46e143470e2009b1b18b9d3b5a
monthiratambumrung <monthira27052546@gmail.com>
2024-06-22 15:48:17 +0700

    Initial commit

---
+++ app/blog/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div class="flex justify-center">
+  <button class="bg-orange-300 ...">
+  <div class="m-6 h-[70px] w-[300px] text-center rounded-">
+    <div> title</div>
+    <div>details</div>
+  </div>
+  </button>
+</div>
+
+    
+  );
+}
